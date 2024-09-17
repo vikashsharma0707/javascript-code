bdf63cff6a00e8bd153383be26ea473658f6da82
vikash sharma <vikashtiwari9893@gmail.com>
2024-09-18 00:55:30 +0530

    arithmetic and assignmetic operatoes

---
+++ arithmetic operators/arithmetic.js
@@ -0,0 +1,67 @@
+
+// addition
+var a= 2;
+var b=3;
+var c=a+b;
+document.write(c,"<br>");
+
+// substraction
+var a= 2;
+var b=3;
+var c=a-b;
+document.write(c,"<br>");
+
+
+// multiply
+var a= 2;
+var b=3;
+var c=a*b;
+document.write(c,"<br>");
+
+
+// divide
+var a= 2;
+var b=3;
+var c=a/b;
+document.write(c,"<br>");
+
+// addition
+var a= 2;
+var b=3;
+var c=a%b;
+document.write(c,"<br>");
+
+
+// squre
+var a= 2;
+var b=3;
+var c=a**b;
+document.write(c,"<br>");
+
+
+// substraction
+var a= 2;
+var b=3;
+var c=a+b;
+document.write(c,"<br>");
+
+// pre increment
+++c;
+document.write(c,"<br>");
+
+// pre decrement
+--c;
+document.write(c,"<br>");
+
+//post increment
+c++;
+document.write(c,"<br>");
+
+// post decrement
+c--;
+document.write(c,"<br>");
+
+
+
+
+
---
+++ assignment operator/assignment.js
@@ -0,0 +1,47 @@
+
+// addition
+var a= 2;
+var b=3;
+ a+=b;
+document.write(a,"<br>");
+
+// substraction
+var a= 2;
+var b=3;
+a-=b;
+document.write(a,"<br>");
+
+
+// multiply
+var a= 2;
+var b=3;
+a*=b;
+document.write(a,"<br>");
+
+
+// divide
+var a= 2;
+var b=3;
+a/=b;
+document.write(a,"<br>");;
+
+// substraction
+var a= 2;
+var b=3;
+a%=b;
+document.write(a,"<br>");
+
+
+// squre
+var a= 2;
+var b=3;
+a**=b;
+document.write(a,"<br>");
+
+
+
+
+
+
+
+
