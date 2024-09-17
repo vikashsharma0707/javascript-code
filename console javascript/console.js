78b0fb07f190e9777f4642ec295081076816ff48
vikash sharma <vikashtiwari9893@gmail.com>
2024-09-18 01:13:10 +0530

    console

---
+++ console javascript/console.js
@@ -0,0 +1,37 @@
+
+
+
+// to check the time of working
+//start here
+console.time("test");
+var a = "hello";
+console.log(a);
+
+let b=10;
+console.log(10+b);
+
+// array
+let c=10;
+console.log([2,3,4]);
+
+//table
+let d=10;
+console.table([2,3,4]);
+
+// error
+let e=10;
+console.error("error function is working");
+
+//warning
+let f=10;
+console.warn("this is just warning")
+
+console.timeEnd("test");
+// timeend
+
+
+// to clear the console
+//console.clear();
+
+
+
