c7238efba70fb2d1a418086c4b80c9cad2bd6283
vikash sharma <vikashtiwari9893@gmail.com>
2024-09-18 00:16:52 +0530

    variables let and const

---
+++ variable/variable1.js
@@ -0,0 +1,28 @@
+
+
+
+
+// let we will only type in one time
+
+
+let a="javascript  ";
+
+a="html "
+document.write(a,"<br/>")
+
+let b="java";
+document.write(b,"<br/>");
+
+
+// const we will only assigend one value
+
+
+const x="hello";
+document.write(x,"<br>");
+
+const y="java"
+document.write(y,"<br>");
+
+
+
+
