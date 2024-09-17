5827f0d192223d5b722670d08891194874831e27
vikash sharma <vikashtiwari9893@gmail.com>
2024-09-17 23:44:01 +0530

    varibale and inline javascript

---
+++ variable/variable.js
@@ -0,0 +1,20 @@
+
+
+
+//var 
+
+var a = "javascrit";
+//document.write(a);
+
+
+var b= "java";
+//document.write(b);
+
+// always print the latest value
+
+a= "html";
+document.write(a);
+
+
+b="css"
+document.write(b);
