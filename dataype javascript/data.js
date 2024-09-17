54ec3f505be0c389150a81852e8eace9a90b30cf
vikash sharma <vikashtiwari9893@gmail.com>
2024-09-18 00:34:04 +0530

    javascript datatype

---
+++ dataype javascript/data.js
@@ -0,0 +1,38 @@
+// string datatype
+let a = "javascript  "
+document.write(a);
+document.write(typeof a,"<br>");
+
+
+// integer datatype
+let  b=10
+document.write(b);
+document.write(typeof b,"<br>");
+
+// boolean datatype
+let  c=true
+document.write(c);
+document.write(typeof c,"<br>");
+
+
+// undefined datatype
+let  d=undefined
+document.write(d);
+document.write(typeof d,"<br>");
+
+// in javascript array takes in the object form
+// array datatype
+let  e=[10,22,33,44,55]
+document.write(e);
+document.write(typeof e);
+
+
+// object datatype
+let  f={
+    name:"java",
+    couse:"javafullstack"
+}
+document.write(f);
+document.write(typeof f);
+
+
