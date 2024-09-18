c45a7ae75a02b3163c9d57909209fdea5d57d116
vikash sharma <vikashtiwari9893@gmail.com>
2024-09-18 08:42:31 +0530

    logical operator in javascript

---
+++ logical operator/logical.js
@@ -0,0 +1,23 @@
+
+// And operator
+var a = 10;
+var b = 15;
+
+if(a< 15  &&  b>10){
+    console.log("and operator is working")
+}
+
+
+
+// Or operator
+var a = 10;
+var b = 15;
+
+if(a> 15  ||  b>10){
+    console.log("and operator is working")
+}
+
+
+
+
+
