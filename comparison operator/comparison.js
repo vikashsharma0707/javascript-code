22f4868f57f7be4b105bc95da585fc986747d824
vikash sharma <vikashtiwari9893@gmail.com>
2024-09-18 08:28:48 +0530

    comparison operators

---
+++ comparison operator/comparison.js
@@ -0,0 +1,56 @@
+
+// equls to
+var a = 10;
+var b = 15;
+
+console.log(a==b);
+
+
+//equal value and equal type
+
+var a = 10;
+var b = "15";
+console.log(a===b);
+
+// not equls to
+var a = 10;
+var b = 15;
+console.log(a!=b);
+
+
+// equls to
+var a = 10;
+var b = "15";
+console.log(a!==b);
+
+// greater than
+var a = 10;
+var b = 15;
+console.log(a>b);
+
+
+// greater than
+var a = 10;
+var b = 15;
+console.log(a<b);
+
+// greater than equals to
+var a = 10;
+var b = 10;
+console.log(a>=b);
+
+//less than equals to
+var a = 10;
+var b = 10;
+console.log(a<=b);
+
+
+
+// null and undefined
+// null equals to undefined gives true both have didnt value so it gives true
+
+console.log(null==undefined);
+
+// but null and undefined data types are not equal so it gives false
+console.log(null===undefined);
+
