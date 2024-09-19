564e748e333b5a7aebdb3edf5e7e881617160c37
vikash sharma <vikashtiwari9893@gmail.com>
2024-09-19 19:36:40 +0530

    switch case

---
+++ conditional statement/switch.js
@@ -0,0 +1,36 @@
+
+
+
+
+const day = 1;
+
+switch(day){
+    case 0:
+    document.write("monday")
+    break;
+
+    case 1:
+    document.write("tuesday")
+    break;
+
+    case 2:
+    document.write("wednesday");
+    break;
+
+    case 3:
+    document.write("thrusday");
+    break;
+
+    case 4:
+    document.write("friday");
+    break;
+
+    case 5:
+    document.write("saturday");
+    break;
+
+    default:
+    document.write("sunday");
+
+
+}
