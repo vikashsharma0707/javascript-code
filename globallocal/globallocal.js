ac10ad012e7ab49a9c868c7a194eb3dc1c9db5fb
vikash sharma <vikashtiwari9893@gmail.com>
2024-09-20 09:12:13 +0530

    global and local variable

---
+++ globallocal/globallocal.js
@@ -0,0 +1,20 @@
+
+let a=10;    // global variable
+
+function hello(){
+    document.write(a);
+}
+
+hello();
+document.write(a);
+
+
+let c=10;
+function  add(){
+  let=  b=5
+}
+
+
+add();
+document.write(c);
+document.write(b);
