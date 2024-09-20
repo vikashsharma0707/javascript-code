3e5e00d02d3fcfdfe4a672d52f807f9da12eda5a
vikash sharma <vikashtiwari9893@gmail.com>
2024-09-20 09:42:46 +0530

    Events in javascript

---
+++ Events javascript/event.js
@@ -0,0 +1,46 @@
+
+
+// onclick
+function hello(){
+    alert("this is onclick event")
+}
+
+// double click
+function hello1(){
+    alert("this is onclick event")
+}
+
+
+function hello2(){
+    alert("this is onclick event")
+}
+
+function hello3(){
+    alert("this is onclick event")
+}
+
+function hello4(){
+    alert("this is onclick event")
+}
+
+function hello5(){
+    alert("this is onclick event")
+}
+
+function hello6(){
+    alert("this is onclick event")
+}
+
+
+
+
+// windows function runs on body tag
+
+function hello7(){
+    alert("this is onkeypress event")
+}
+
+function hello8(){
+    alert("this is onresize event")
+}
+
